d62fd9929326ba7cc9d9f83046683507f909b92a
keanejoel <joelkeane3@gmail.com>
2020-04-28 23:27:26 -0400

    initial commit

---
+++ api/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { Client } = require('pg');
+const { uuid } = require('uuidv4');
+const bcrypt = require('bcrypt');
+
+require('dotenv').config();
+
+var app = express();
+
+app.get('/', (req, res) => {
+  const client = new Client();
+  const query = {
+    text: `SELECT * FROM users`
+  }
+  client.connect()
+    // when connection is made the following code will run ...
+    .then(() => {
+      console.log('connected to database');
+      return client.query(query);
+    })
+    .then((results) => {
+      res.status(200)
+      .json(results.rows);
+    })
+    .catch((err) => {
+      console.log('error', err);
+      res.send('Something went wrong...');
+    });
+});
+
+app.post('/', (req, res) => {
+  const client = new Client();
+  const saltRounds = 8;
+
+  const hash = bcrypt.hashSync(req.body.password, saltRounds)
+  client.connect()
+    .then(() => {
+      // do query stuff
+      const sql = 'INSERT INTO users (id, email, password) VALUES ($1, $2, $3)'
+      const params = [uuid(), req.body.email, hash];
+      return client.query(sql, params);
+    })
+    .then(() => {
+      res.send('User has been added!');
+    })
+    .catch((err) => {
+      console.log('error', err);
+      res.send('Something went wrong...');
+    });
+});
+
+
+module.exports = {
+  path: '/api/users',
+  handler: app
+}
---
+++ pages/register.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <div class="register-wrapper">
+      <p class="login">Already have an account? Login <nuxt-link to="/login" >here</nuxt-link></p>
+      <p class="error" v-if="error">{{ error }}</p>
+      <div class="container">
+        <div class="row">
+          <div class="cl-table">
+            <div class="cell">
+              <div class="col-md-12">
+                <img src="/cl-logo.svg" alt="CheckLink" />
+              </div>
+              <div class="col-md-12">
+                <div class="create-user" style="margin-top: 2em;">
+                    <h2>Sign Up</h2>
+                    <form action="">
+                      <input type="text" v-model="email" placeholder="Email" autocomplete="off" /><br>
+                      <input type="password" v-model="password" placeholder="Password" autocomplete="new-password" /><br>
+                      <nuxt-link v-on:click.native="createUser()" to="/confirmation" class="nuxt-link">Register</nuxt-link>
+                    </form>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="row">
+        <div class="col-md-12">
+          <div class="user"
+          v-for="(theUser, index) in users"
+          v-bind:item="theUser.username"
+          v-bind:index="index"
+          v-bind:key="theUser.user_id"
+          >
+            {{theUser.username}} - {{theUser.id}} - {{theUser.password}} - {{theUser.email}}
+          </div>
+        </div>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+  import UserService from '../api/UserService';
+
+  export default {
+    name: 'Register',
+    data() {
+      return {
+        users: [],
+        id: '',
+        email: '',
+        password: '',
+        error: null
+      }
+    },
+    async created() {
+      try {
+        this.users = await UserService.getUsers();
+      } catch(err) {
+        this.error = err.message;
+      }
+    },
+    // methods: {
+    //   async createUser() {
+    //     await UserService.insertUser(this.id, this.email, this.password, this.first, this.last, this.username);
+    //   }
+    // }
+  }
+</script>
+
+<style scoped>
+.nuxt-link {
+  display: inline-block;
+  margin-top: 2em;
+}
+img {
+  width: 175px;
+}
+input {
+    width: 200px;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+    font-size: 14px;
+    margin-bottom: 2em;
+    margin-top: 1em;
+    border-left: none !important;
+    border-right: none !important;
+    border-top: none !important;
+    border-bottom: 1px solid #000;
+}
+p.login {
+  position: absolute;
+  right: 30px;
+  top: 30px;
+  font-size: 13px;
+}
+</style>
