d4297d325c03083258d6475d831d0f6c67263202
keanejoel <joelkeane3@gmail.com>
2020-05-05 14:55:01 -0400

    update to post request

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Client } = require('pg');
-const { uuid } = require('uuidv4');
+//const { uuid } = require('uuidv4');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 
@@ -42,7 +42,7 @@ app.post('/', (req, res) => {
     .then(() => {
       // do query stuff
       const sql = 'INSERT INTO users (id, email, password) VALUES ($1, $2, $3)'
-      const params = [uuid(), req.body.email, hash];
+      const params = [req.body.id, req.body.email, hash];
       return client.query(sql, params);
     })
     .then(() => {
